229617f6aca35096f010675f38b2ed2ae7a269a6
ChandruJ2004 <chandruappu2004@gmail.com>
2024-10-24 16:12:30 +0530

    newly commited

---
+++ TNS/src/day1/Demo.java
@@ -0,0 +1,15 @@
+package day1;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter the values of a and b");
+		a=s.nextInt();
+		b=s.nextInt();
+		c=a+b;
+		System.out.println("Addition of a="+a+" and b="+b+" is :"+c);
+		System.out.println("hello");
+	}
+}
