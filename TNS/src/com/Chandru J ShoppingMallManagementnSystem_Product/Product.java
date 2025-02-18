9be2d699dd64985aebda7511f9f122f28cd228e4
ChandruJ2004 <chandruappu2004@gmail.com>
2025-02-18 14:37:43 +0530

    Add files via upload

---
+++ TNS/src/com/Chandru J ShoppingMallManagementnSystem_Product/Product.java
@@ -0,0 +1,67 @@
+package com.tns.shoppingmallservice;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String category;
+    private double price;
+    private int stockQuantity;
+
+    public Product() {
+        super();
+    }
+
+    public Product(Long id, String name, String category, double price, int stockQuantity) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.price = price;
+        this.stockQuantity = stockQuantity;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getStockQuantity() {
+        return stockQuantity;
+    }
+
+    public void setStockQuantity(int stockQuantity) {
+        this.stockQuantity = stockQuantity;
+    }
+}
